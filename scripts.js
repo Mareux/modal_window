2298f89e82108e286f96a8d40e43c881a21edaf8
Mareux <mareuxbaka@gmail.com>
2019-10-05 14:20:18 +0300

    delete, done switch

--- scripts.js
+++ scripts.js
@@ -1,18 +1,90 @@
 let modal = document.getElementById("modal");
 let triggerButton = document.getElementById("trigger");
-let close = document.getElementsByClassName("close")[0];
+let cancelButton = document.getElementById("cancel");
+let form = document.getElementById("todoList");
 
-triggerButton.onclick = function() {
-    modal.style.display = "block";
+triggerButton.onclick = function () {
+    modal.style.display = "flex";
 };
 
-close.onclick = function() {
+cancelButton.onclick = function () {
     modal.style.display = "none";
+};
+
+window.onload = renderItems;
+
+function updateTodos(todoList) {
+    localStorage.setItem("todo-list", JSON.stringify(todoList));
+    renderItems();
 }
 
-window.onclick = function(event) {
-    if (event.targer == modal){
+function loadTodos() {
+    return JSON.parse(localStorage.getItem("todo-list") || "[]");
+}
+
+function renderItems() {
+
+    document.getElementsByClassName("root")[0].innerHTML = "";
+
+    loadTodos()
+        .forEach((item) => {
+            const div = document.createElement('div');
+            div.innerHTML = `
+                <span onclick='deleteItem(${item.id})'>x</span>
+                <p>${item.title}</p>
+                <p>${item.priority}</p>
+                <p>${item.description}</p>
+                <p>${item.deadline}</p>
+                <button onclick="switchDone(${item.id})">
+                    ${item.done ? "Mark as undone" : "Mark as done"}
+                </button>
+                <button>Edit</button>
+            `;
+            div.className = "todo";
+            if (item.done)
+                div.className += " done";
+            document.getElementsByClassName("root")[0].appendChild(div);
+        });
+}
+
+function switchDone(id) {
+
+    const todoList = loadTodos();
+    const todo = todoList.find((item) => (item.id === id));
+    todo.done = !todo.done;
+
+    updateTodos(todoList);
+
+}
+
+function deleteItem(id) {
+
+    const newTodoList = loadTodos().filter((item) => (item.id !== id));
+    updateTodos(newTodoList);
+}
+
+function submitForm() {
+    const todo = {
+        id: Date.now(),
+        title: form["title"].value,
+        description: form["description"].value,
+        priority: form["priority"].value,
+        deadline: form["deadline"].value,
+        completed: false
+    };
+
+    const todoList = loadTodos();
+    todoList.push(todo);
+    updateTodos(todoList);
+
+    modal.style.display = "none";
+
+    return false;
+}
+
+window.onclick = function (event) {
+    if (event.target === modal) {
         modal.style.display = "none";
     }
-}
+};
 
